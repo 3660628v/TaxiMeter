12a2f909a817ef51d6a8720319946450fd019629
hicder <hieup@outlook.com>
2013-06-14 16:49:44 +0700

    Initial Commit

---
+++ TaxiMeter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TaxiMeter
+//
+//  Created by Hieu on 6/14/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
