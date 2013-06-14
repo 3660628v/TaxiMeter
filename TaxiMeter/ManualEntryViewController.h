ee7f1450ddacd71e4d6379e528892b9af45fb589
hicder <hieup@outlook.com>
2013-06-14 23:36:21 +0700

    Taxi Meter

---
+++ TaxiMeter/ManualEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ManualEntryViewController.h
+//  TaxiMeter
+//
+//  Created by Hieu on 6/14/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManualEntryViewController : UIViewController
+
+@end
---
+++ TaxiMeter/ManualEntryViewController.m
@@ -0,0 +1,38 @@
+//
+//  ManualEntryViewController.m
+//  TaxiMeter
+//
+//  Created by Hieu on 6/14/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import "ManualEntryViewController.h"
+
+@interface ManualEntryViewController ()
+
+@end
+
+@implementation ManualEntryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
