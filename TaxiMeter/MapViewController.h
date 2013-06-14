1c1e39f911dfa44aec16f088d9969191a346ca7d
hicder <hieup@outlook.com>
2013-06-14 17:45:00 +0700

    Taxi Meter

---
+++ TaxiMeter/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  TaxiMeter
+//
+//  Created by Hieu on 6/14/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
